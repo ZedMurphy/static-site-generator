e13185fe97a13fa27018c52a17a735444cf7c886
ZedMurphy <Spreading.Chaoz@gmail.com>
2025-05-21 15:55:15 +0200

    Now TextNode can be converted to HTMLNode.

--- src/test_testnode.py
+++ src/test_testnode.py
@@ -1,26 +1,41 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node = TextNode("This is a text node", TextType.BOLD_TEXT)
-        node2 = TextNode("This is a text node", TextType.BOLD_TEXT)
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
-        node3 = TextNode("I am Groot!", TextType.ITALIC_TEXT, "http://my-site.de")
-        node4 = TextNode("I am Groot!", TextType.ITALIC_TEXT)
+        node3 = TextNode("I am Groot!", TextType.ITALIC, "http://my-site.com")
+        node4 = TextNode("I am Groot!", TextType.ITALIC)
         self.assertNotEqual(node3, node4)
         node5 = TextNode("Exactly, Watson!", TextType.IMAGE, None)
         node6 = TextNode("Extactly, Watson!", TextType.IMAGE, None)
         self.assertNotEqual(node5, node6)
-        node7 = TextNode("Don't panic!", TextType.CODE_TEXT, None)
-        node8 = TextNode("Don't panic!", TextType.CODE_TEXT)
+        node7 = TextNode("Don't panic!", TextType.CODE, None)
+        node8 = TextNode("Don't panic!", TextType.CODE)
         self.assertEqual(node7, node8)
-        node9 = TextNode("Hello World!", TextType.NORMAL_TEXT)
-        node10 =TextNode("Hello World!", TextType.BOLD_TEXT)
+        node9 = TextNode("Hello World!", TextType.TEXT)
+        node10 =TextNode("Hello World!", TextType.BOLD)
         self.assertNotEqual(node9, node10)
 
+    def test_txt_node_to_html_node(self):
+        node1 = TextNode("This is a text node", TextType.TEXT)
+        html_node1 = text_node_to_html_node(node1)
+        self.assertEqual(html_node1.tag, None)
+        self.assertEqual(html_node1.value, "This is a text node")
+        node2 = TextNode("This is a pretty image.", TextType.IMAGE, "https://my_image.com")
+        html_node2 = text_node_to_html_node(node2)
+        self.assertEqual(html_node2.tag, "img")
+        self.assertEqual(html_node2.value, "")
+        self.assertEqual(html_node2.props, {"src": "https://my_image.com", "alt": "This is a pretty image."})
+        node3 = TextNode("Click me!", TextType.LINK, "https://my_site.com")
+        html_node3 = text_node_to_html_node(node3)
+        self.assertEqual(html_node3.tag, "a")
+        self.assertEqual(html_node3.value, "Click me!")
+        self.assertEqual(html_node3.props, {"href": "https://my_site.com"})
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,12 +1,13 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
-    NORMAL_TEXT = "normal"
-    BOLD_TEXT = "bold"
-    ITALIC_TEXT = "italic"
-    CODE_TEXT = "code"
-    LINK = "link"
-    IMAGE = "image"
+    TEXT = ""
+    BOLD = "b"
+    ITALIC = "i"
+    CODE = "code"
+    LINK = "a"
+    IMAGE = "img"
 
 class TextNode:
     def __init__(self, text, text_type, url=None):
@@ -19,3 +20,22 @@ class TextNode:
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
+
+def text_node_to_html_node(text_node):
+    
+    match text_node.text_type:
+        case (TextType.TEXT):
+            return LeafNode(None, text_node.text)
+        case (TextType.BOLD):
+            return LeafNode("b", text_node.text)
+        case (TextType.ITALIC):
+            return LeafNode("i", text_node.text)
+        case (TextType.CODE):
+            return LeafNode("code", text_node.text)
+        case (TextType.LINK):
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case (TextType.IMAGE):
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise Exception("No valid text type given.")
