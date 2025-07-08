f31093ce52d5bddaf67a70e50d1aea5bbe3f7396
ZedMurphy <Spreading.Chaoz@gmail.com>
2025-07-08 14:43:02 +0200

    now subpages can be converted, too

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from pathlib import Path
 
 from copystatic import copy_files_recursive
 from markdowntonode import markdown_to_html_node
@@ -7,6 +8,8 @@ from markdowntonode import markdown_to_html_node
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 
 def main():
@@ -17,7 +20,9 @@ def main():
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
-    generate_page("content/index.md", "template.html", "public/index.html")
+    print("Generating content...")
+    #generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
 
 def extract_title(markdown):
     single_lines = markdown.split("\n")
@@ -56,6 +61,22 @@ def generate_page(from_path, template_path, dest_path):
 
     with open(dest_path, 'w') as f:
         f.write(new_page)
+
+#def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    #print(f"Generating pages from {dir_path_content} to {dest_dir_path} using {template_path}...")
+    
+    #list_of_directories = os.listdir(dir_path_content)
+    #for entry in list_of_directories:
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path)
         
 
 main()
